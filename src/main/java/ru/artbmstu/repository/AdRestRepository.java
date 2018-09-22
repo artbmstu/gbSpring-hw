c37c05939c07f0433a4c1cfad2347eb0d46d2c73
Aleksei <art.bmstu@gmail.com>
2018-09-22 14:28:32 +0300

    [+] rest resources

--- src/main/java/ru/artbmstu/repository/AdRestRepository.java
+++ src/main/java/ru/artbmstu/repository/AdRestRepository.java
@@ -4,13 +4,10 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import ru.artbmstu.dto.AdDTO;
-import ru.artbmstu.model.AdEntity;
 
 @Repository(AdRepository.NAME)
 public interface AdRestRepository extends PagingAndSortingRepository<AdDTO, String> {
 
     @NonNull
     String NAME = "adRestRepository";
-
-
 }
